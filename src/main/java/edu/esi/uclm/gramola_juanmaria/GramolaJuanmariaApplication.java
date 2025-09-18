fb8c1ab2abfa6414e6cfd250a05733135c8252ad
juanmariabravo <JuanMaria.Bravo@alu.uclm.es>
2025-09-18 17:27:37 +0200

    Empty Maven Java Project Upload

---
+++ src/main/java/edu/esi/uclm/gramola_juanmaria/GramolaJuanmariaApplication.java
@@ -0,0 +1,13 @@
+package edu.esi.uclm.gramola_juanmaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramolaJuanmariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GramolaJuanmariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/esi/uclm/gramola_juanmaria/GramolaJuanmariaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.esi.uclm.gramola_juanmaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramolaJuanmariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
