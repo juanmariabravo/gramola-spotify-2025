600ba5ef9a6eb9b47bd771929bfa4af7b53be443
juanmariabravo <JuanMaria.Bravo@alu.uclm.es>
2025-10-09 21:36:53 +0200

    Añadido código del profesor en SpotiService

--- backend/src/main/java/edu/esi/uclm/gramola_juanmaria/services/SpotiService.java
+++ backend/src/main/java/edu/esi/uclm/gramola_juanmaria/services/SpotiService.java
@@ -1,8 +1,39 @@
 package edu.esi.uclm.gramola_juanmaria.services;
 
+import java.net.http.HttpHeaders;
+
 public class SpotiService {
     
     public SpotiToken getAuthorizationToken(String code, String clientId) {
-        return null;
+        User user = this.UserService.getUserByClientId(clientId);
+        String clientSecret = user.getClientSecret();
+
+        // Llamar a SpotiAPI para obtener el token de autorización
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("code", code);
+        form.add("grant_type", "authorization_code");
+        form.add("redirect_uri", "http://127.0.0.1:4200/callback");
+
+        // Realizar la solicitud a la API de Spotify
+        String header = this.basicAuth(clientId, clientSecret);
+
+        String url = this.tokenUrl + "/api/token";
+        SpotiToken token = restClient.post()
+            .uri(url)
+            .header(HttpHeaders.AUTHORIZATION, header)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(form)
+            .retrieve()
+            .body(SpotiToken.class);
+
+            user.setSpotiSimpleToken(token);
+            return token;
+        }
+
+    private String basicAuth(String clientId, String clientSecret) {
+        String pairs = clientId + ":" + clientSecret;
+        String encoded = Base64.getEncoder().encodeToString(pairs.getBytes());
+        return "Basic " + encoded;
     }
+
 }
