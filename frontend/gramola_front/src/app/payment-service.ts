81546c717a5f42a1c2eb2f7ed58123be847d61d9
juanmariabravo <JuanMaria.Bravo@alu.uclm.es>
2025-10-15 17:27:05 +0200

    fix: [PaymentService] change prepay method to use GET instead of POST and remove console log

--- frontend/gramola_front/src/app/payment-service.ts
+++ frontend/gramola_front/src/app/payment-service.ts
@@ -9,10 +9,9 @@ export class PaymentService {
   constructor(private client: HttpClient) { }
 
   prepay(): Observable<any> {
-    console.log("prepay service");
     // la siguiente URL no debería estar "hardcodeada", debería venir de una variable de entorno
 //  return this.client.post(`${environment.apiUrl}/payments/prepay`, {}, { 
-    return this.client.post('http://localhost:8080/payments/prepay', {}, {
+    return this.client.get('http://localhost:8080/payments/prepay', {
       withCredentials: true, // para enviar cookies
       observe: 'response', // queremos observar la respuesta completa
       responseType: 'text' // el cuerpo de la respuesta es texto
