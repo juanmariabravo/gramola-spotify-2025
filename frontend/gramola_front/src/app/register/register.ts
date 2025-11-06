f73956b1f962accf8afd05760446172b143ecf9d
juanmariabravo <JuanMaria.Bravo@alu.uclm.es>
2025-11-06 16:35:42 +0100

    fix: Feedback del registro arreglado feat: Refactor registration form to use Reactive Forms; add validation messages for email and password fields

--- frontend/gramola_front/src/app/register/register.ts
+++ frontend/gramola_front/src/app/register/register.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importa CommonModule
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../user-service';
 import { Router } from '@angular/router';
-import { waitForAsync } from '@angular/core/testing';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
 export class Register {
+  registerForm: FormGroup;
 
   barName? : string
   email? : string
@@ -20,21 +20,38 @@ export class Register {
   pwd2? : string
   clientId? : string
   clientSecret? : string
+  
+  registroExitoso? : boolean
 
-  constructor(private service : UserService, private router: Router) { }
+  constructor(private fb: FormBuilder, private service : UserService, private router: Router) {
+    this.registerForm = this.fb.group({
+      barName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      pwd1: ['', [Validators.required, Validators.minLength(8)]],
+      pwd2: ['', [Validators.required, Validators.minLength(8)]],
+      clientId: [''],
+      clientSecret: ['']
+    });
+  }
 
-  registroExitoso: boolean | null = null;
-
-  registrar() {
-    if (this.pwd1 != this.pwd2) {
-      console.error('Las contraseñas no coinciden');
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
       return;
     }
+
+    this.barName = this.registerForm.value.barName;
+    this.email = this.registerForm.value.email;
+    this.pwd1 = this.registerForm.value.pwd1;
+    this.pwd2 = this.registerForm.value.pwd2;
+    this.clientId = this.registerForm.value.clientId;
+    this.clientSecret = this.registerForm.value.clientSecret;
 
     this.service.register(this.barName!, this.email!, this.pwd1!, this.pwd2!, this.clientId!, this.clientSecret!).subscribe(
       ok => {
         console.log('Registro exitoso', ok);
         this.registroExitoso = true;
+        this.registerForm.reset();
       },
       error => {
         console.error('Error en el registro', error);
